fcb0e43b58646406268b7f3b6728b913e4a3101a
John Castro de Sousa <37676163+johnsousac@users.noreply.github.com>
2024-01-11 22:19:56 -0300

    criacao de componente de listagem de pensamentos e configuracao de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // quando for requisitado somente a url sem uri será direcionado para o componente de listar pensamentos
+  { path: '', redirectTo: 'listarPensamento', pathMatch: 'full' },
+  { path: 'criarPensamento', component: CriarPensamentoComponent },
+  { path: 'listarPensamento', component: ListarPensamentoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
