916903cd6129811f934f1ae9cb55628407cad0f0
John Castro de Sousa <37676163+johnsousac@users.noreply.github.com>
2024-01-13 22:33:08 -0300

    listagem de pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit veritatis, quae quam optio sint assumenda velit deserunt fuga. Eos, neque. Optio ipsum reprehenderit impedit laboriosam qui ratione dolor, sint quidem porro laudantium ipsam recusandae quas beatae libero est excepturi omnis asperiores mollitia esse magnam, dolores veritatis aut quod? Ad, necessitatibus?',
+      autoria: 'Grecia',
+      modelo: 'modelo1'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
