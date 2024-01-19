07efb64b04f83cd8b482794b5fabd00068005da6
John Castro de Sousa <37676163+johnsousac@users.noreply.github.com>
2024-01-18 21:42:33 -0300

    finalizacao do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   // quando for requisitado somente a url sem uri será direcionado para o componente de listar pensamentos
   { path: '', redirectTo: 'listarPensamento', pathMatch: 'full' },
   { path: 'criarPensamento', component: CriarPensamentoComponent },
-  { path: 'listarPensamento', component: ListarPensamentoComponent }
+  { path: 'listarPensamento', component: ListarPensamentoComponent },
+  { path: 'pensamentos/excluirPensamento/:id', component: ExcluirPensamentoComponent },
+  { path: 'pensamentos/editarPensamento/:id', component: EditarPensamentoComponent }
 ];
 
 @NgModule({
